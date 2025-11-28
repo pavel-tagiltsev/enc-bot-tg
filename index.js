25e48613c8ae0827995e40ca709141f9a738f8ca
Pavel Tagiltsev <pavel.tagiltsef@gmail.com>
2025-11-28 12:21:32 +0700

    feat(index): prepare data for template and render html as response

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 import { Telegraf } from 'telegraf';
 import { CronJob } from 'cron';
 import moyKlassAPI from './MoyKlassAPI.js';
+import View from './View.js';
+import Time from './Time.js';
 
 dotenv.config();
 
@@ -48,24 +50,49 @@ class TgBot {
     });
 
     const overduePaymentInvoices = invoicesRes.invoices.filter((invoice) => {
-      return invoice.price !== invoice.payed && new Date(invoice.payUntil) < new Date();
+      const isDebt = invoice.price !== invoice.payed;
+      const isOverdue = new Date(invoice.payUntil) < new Date(Time.formatYMD(new Date()));
+
+      return isDebt && isOverdue;
     });
+
+    const overduePaymentUsersIds = overduePaymentInvoices.map((invoice) => invoice.userId);
+    const uniqueOverduePaymentUsersIds = [...new Set(overduePaymentUsersIds)];
 
     const usersRes = await moyKlassAPI.get('/users', {
       params: {
-        userIds: [...new Set(overduePaymentInvoices.map((invoice) => invoice.userId))],
+        userIds: uniqueOverduePaymentUsersIds,
       }
     });
     await moyKlassAPI.revokeToken();
 
-    const users = [...new Set(usersRes.users.map((user) => user.name))];
+    const templateData = usersRes.users.reduce((acc, user) => {
+      const userInvoices = overduePaymentInvoices.filter(invoice => invoice.userId === user.id);
+      const userTotalDebt = userInvoices.reduce((sum, invoice) => sum + (invoice.price - invoice.payed), 0);
+      const userPayUntilDates = userInvoices.map(invoice => new Date(invoice.payUntil));
+      const userEarliestPayUntilDate = Time.formatYMD(new Date(Math.min(...userPayUntilDates)));
+
+      acc.users.push({
+        id: user.id,
+        name: user.name,
+        totalDebt: userTotalDebt,
+        earliestPayUntil: userEarliestPayUntilDate
+      });
+
+      acc.stats.totalUsers += 1;
+      acc.stats.totalDebt += userTotalDebt;
+
+      return acc;
+    }, { users: [], stats: { totalUsers: 0, totalDebt: 0 } });
+
+    const template = View.renderSubscriptionDebtNotificationTemplate(templateData);
 
     if (ctx) {
-      ctx.reply(users.join(','));
+      ctx.replyWithHTML(template);
       return;
     }
 
-    this.bot.telegram.sendMessage(process.env.DEVELOPER_CHAT_ID, users);
+    this.bot.telegram.sendMessage(process.env.DEVELOPER_CHAT_ID, template, { parse_mode: 'HTML' });
   }
 }
 
