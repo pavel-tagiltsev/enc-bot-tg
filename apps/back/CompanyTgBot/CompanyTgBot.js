8bb3250b05eb0e111d0971d05ff014df4670778d
Pavel Tagiltsev <pavel.tagiltsef@gmail.com>
2026-01-01 13:11:07 +0700

    feat(index): restructuring executeUnmarkedLessonsNotification methods

--- apps/back/CompanyTgBot/CompanyTgBot.js
+++ apps/back/CompanyTgBot/CompanyTgBot.js
@@ -4,6 +4,7 @@ import { CronJob } from 'cron';
 import moyKlassAPI from './Helpers/MoyKlassAPI.js';
 import View from './Helpers/View.js';
 import Time from './Helpers/Time.js';
+import UnmarkedLessonsNotification from './UseCases/UnmarkedLessonsNotification.js';
 
 dotenv.config();
 const ADMIN_IDS = [Number(process.env.DEVELOPER_CHAT_ID), Number(process.env.ADMIN_CHAT_ID)];
@@ -125,15 +126,15 @@ export default class CompanyTgBot {
   }
   executeUnmarkedLessonsNotification = async (ctx = null) => {
     console.log('CompanyTgBot.executeUnmarkedLessonsNotification:start');
+    const callback = (templateData) => {
+      this.sendHTMLMessage({
+        template: View.renderUnmarkedLessonsNotificationTemplate(templateData),
+        consoleMsg: 'CompanyTgBot.executeUnmarkedLessonsNotification:end',
+        ctx
+      });
+    }
 
-    const data = await this.getUnmarkedLessonsNotificationData();
-    const templateData = this.buildUnmarkedLessonsNotificationTemplateData(data);
-
-    this.sendHTMLMessage({
-      template: View.renderUnmarkedLessonsNotificationTemplate(templateData),
-      consoleMsg: 'CompanyTgBot.executeUnmarkedLessonsNotification:end',
-      ctx
-    });
+    await UnmarkedLessonsNotification.execute(callback);
   }
 
   sendHTMLMessage = ({template, consoleMsg, ctx = null}) => {
@@ -147,89 +148,5 @@ export default class CompanyTgBot {
       this.bot.telegram.sendMessage(adminId, template, { parse_mode: 'HTML' });
     }
     console.log(consoleMsg);
-  }
-
-  getUnmarkedLessonsNotificationData = async () => {
-    let data = {};
-
-    await moyKlassAPI.setToken();
-    const { lessons } = await moyKlassAPI.get('/lessons', {
-      params: {
-        date: ['2025-09-01', Time.formatYMD(new Date())],
-        includeRecords: true,
-        limit: 500,
-        sort: 'date',
-        sortDirection: 'desc'
-      }
-    });
-
-    data.lessons = lessons.filter((lesson) => {
-      const { records, comment, status } = lesson;
-      const isHasStatus = status;
-      const isNoVisits = records.every(({ visit }) => !visit);
-      const isNoReasonComment = !comment || !comment.trim().startsWith('#');
-
-      return isNoVisits && isNoReasonComment && isHasStatus;
-    });
-
-    const { users } = await moyKlassAPI.get('/users', {
-      params: {
-        userIds: [...new Set(data.lessons.flatMap(({ records }) => records.flatMap(({ userId }) => userId)))],
-        limit: 500,
-      }
-    });
-
-    data.users = users;
-
-    data.classes = await moyKlassAPI.get('/classes', {
-      params: {
-        classId: [...new Set(data.lessons.map(({ classId }) => classId))]
-      }
-    });
-
-    const unmarkedLessonsTeacherIds = data.lessons.flatMap(({ teacherIds }) => teacherIds);
-    const uniqueUnmarkedLessonsTeacherIds = [...new Set(unmarkedLessonsTeacherIds)];
-
-    const managers = await moyKlassAPI.get('/managers');
-
-    data.managers = managers.filter(({ id }) => uniqueUnmarkedLessonsTeacherIds.includes(id));
-    await moyKlassAPI.revokeToken();
-
-    return data;
-  }
-
-  buildUnmarkedLessonsNotificationTemplateData = (data) => {
-    const { lessons, managers, classes, users } = data;
-
-    return managers.reduce((acc, manager) => {
-      const { teachers, stats } = acc;
-      const { id, name } = manager;
-      const teacherLessons = lessons.filter(({ teacherIds }) => teacherIds.includes(id));
-
-      teachers.push({
-        id,
-        name,
-        totalLessons: teacherLessons.length,
-        lessons: teacherLessons.map((lesson) => {
-          const { date, beginTime, classId, records } = lesson;
-          const cls = classes.find(({ id }) => id === classId);
-          const isIndividual = cls.courseId === 0;
-
-          return {
-            date,
-            beginTime,
-            classId: classId,
-            className: cls.name,
-            userId: isIndividual ? records[0].userId : null,
-            userName: isIndividual ? users.find(({ id }) => id === records[0].userId).name : null,
-          }
-        })
-      });
-
-      stats.totalTeachers += 1;
-      stats.totalLessons += teacherLessons.length;
-
-      return acc;
-    }, { teachers: [], stats: { totalTeachers: 0, totalLessons: 0 } });
   }
 }
