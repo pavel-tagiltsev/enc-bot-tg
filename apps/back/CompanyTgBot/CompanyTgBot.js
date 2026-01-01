39a15abe2eb55f4837cb9fa256e2bc7e1cadb0f8
Pavel Tagiltsev <pavel.tagiltsef@gmail.com>
2026-01-01 15:44:25 +0700

    refactor(index): restructuring notifications and command logic

--- apps/back/CompanyTgBot/CompanyTgBot.js
+++ apps/back/CompanyTgBot/CompanyTgBot.js
@@ -8,15 +8,31 @@ import SubscriptionDebtNotification from './UseCases/SubscriptionDebtNotificatio
 dotenv.config();
 const ADMIN_IDS = [Number(process.env.DEVELOPER_CHAT_ID), Number(process.env.ADMIN_CHAT_ID)];
 
-const notifications = {
+const notificationsConfig = {
   subscriptionDebt: {
     service: SubscriptionDebtNotification,
     render: View.renderSubscriptionDebtNotificationTemplate,
+    cronTime: '0 9 * * 1-5',
+    adminOnly: true,
+    command: 'subscription_debts',
+    description: 'Показать все задолженности по ученикам'
+  },
+  AllUnmarkedLessons: {
+    service: UnmarkedLessonsNotification,
+    render: View.renderUnmarkedLessonsNotificationTemplate,
+    cronTime: '0 9 * * 1-5',
+    adminOnly: true,
+    command: 'all_unmarked_lessons',
+    description: 'Показать неотмеченные уроки по учителям'
   },
   unmarkedLessons: {
     service: UnmarkedLessonsNotification,
     render: View.renderUnmarkedLessonsNotificationTemplate,
-  },
+    cronTime: null,
+    adminOnly: false,
+    command: 'unmarked_lessons',
+    description: 'Показать мои неотмеченные уроки'
+  }
 };
 
 export default class CompanyTgBot {
@@ -33,71 +49,70 @@ export default class CompanyTgBot {
 
   initNotifications = () => {
     console.log('CompanyTgBot.initNotifications');
-    CronJob.from({
-      cronTime: '0 9 * * 1-5',
-      onTick: async () => {
-        await this.executeNotification('subscriptionDebt');
-        await this.executeNotification('unmarkedLessons');
-      },
-      start: true,
-      timeZone: 'Europe/Moscow'
+
+    Object.values(notificationsConfig).forEach((cfg) => {
+      if (!cfg.cronTime) {
+        return
+      }
+
+      CronJob.from({
+        cronTime: cfg.cronTime,
+        onTick: async () => {
+          await this.sendNotification(cfg, null);
+        },
+        start: true,
+        timeZone: 'Europe/Moscow'
+      });
     });
   }
 
   initCommands = async () => {
     console.log('CompanyTgBot.initCommands');
-    const userCommands = [
-      { command: 'start', description: 'Запуск бота' },
-      { command: 'help', description: 'Помощь' },
-      { command: 'unmarked_lessons', description: 'Показать мои неотмеченные уроки' },
-      { command: 'all_unmarked_lessons', description: 'Показать неотмеченные уроки по учителям' },
-      { command: 'subscription_debts', description: 'Показать все задолженности по ученикам' },
-    ];
 
-    await this.bot.telegram.setMyCommands(userCommands, {
-      scope: { type: 'all_private_chats' }
-    });
+    const userCommands = Object.values(notificationsConfig)
+      .map((cfg) => ({
+        command: cfg.command,
+        description: cfg.description
+      }));
+
+    await this.bot.telegram.setMyCommands(userCommands, { scope: { type: 'all_private_chats' } });
 
     const adminOnly = async (ctx, next) => {
       if (!ctx.from || !ADMIN_IDS.includes(ctx.from.id)) {
         await ctx.reply('⛔ Нет доступа');
         return;
       }
-
       return next();
     }
 
+    Object.values(notificationsConfig).forEach(cfg => {
+      const middlewares = [];
+      if (cfg.adminOnly) {
+        middlewares.push(adminOnly);
+      }
+      middlewares.push((ctx) => this.sendNotification(cfg, ctx));
+      this.bot.command(cfg.command, ...middlewares);
+    });
+
     this.bot.start((ctx) => ctx.reply('Запуск бота'));
     this.bot.help((ctx) => ctx.reply('Помощь'));
-    this.bot.command('unmarked_lessons', (ctx) => ctx.reply('Показать мои неотмеченные уроки'));
-    this.bot.command('subscription_debts', adminOnly, (ctx) => this.executeNotification('subscriptionDebt', ctx));
-    this.bot.command('all_unmarked_lessons', adminOnly, (ctx) => this.executeNotification('unmarkedLessons', ctx));
   }
 
-  executeNotification = async (key, ctx = null) => {
-    const { service, render } = notifications[key];
+  sendNotification = async (cfg, ctx = null) => {
+    console.log(`CompanyTgBot.${cfg.command}:start`);
 
-    console.log(`CompanyTgBot.${key}:start`);
+    await cfg.service.execute((templateData) => {
+      const html = cfg.render(templateData);
 
-    await service.execute((templateData) => {
-      this.sendHTMLMessage({
-        template: render(templateData),
-        consoleMsg: `CompanyTgBot.${key}:end`,
-        ctx,
-      });
+      if (ctx) {
+        ctx.replyWithHTML(html);
+      } else {
+        for (const adminId of ADMIN_IDS) {
+          this.bot.telegram.sendMessage(adminId, html, { parse_mode: 'HTML' });
+        }
+      }
+
+      console.log(`CompanyTgBot.${cfg.command}:end`);
     });
-  };
-
-  sendHTMLMessage = ({template, consoleMsg, ctx = null}) => {
-    if (ctx) {
-      ctx.replyWithHTML(template);
-      console.log(consoleMsg);
-      return;
-    }
-
-    for (const adminId of ADMIN_IDS) {
-      this.bot.telegram.sendMessage(adminId, template, { parse_mode: 'HTML' });
-    }
-    console.log(consoleMsg);
   }
 }
