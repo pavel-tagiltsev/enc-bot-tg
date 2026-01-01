09749355321bff62bed7d12d7de9a5472947a6b7
Pavel Tagiltsev <pavel.tagiltsef@gmail.com>
2026-01-01 17:16:28 +0700

    feat(server): adding graceful shutdown

--- server.js
+++ server.js
@@ -5,3 +5,12 @@ dotenv.config();
 
 const botInstance = new CompanyTgBot(process.env.TELEGRAM_TOKEN);
 await botInstance.launch();
+
+const shutdown = async (signal) => {
+  console.log(`Shutdown: ${signal}`);
+  await botInstance.stop(signal);
+  process.exit(0);
+};
+
+process.once('SIGINT', shutdown);
+process.once('SIGTERM', shutdown);
