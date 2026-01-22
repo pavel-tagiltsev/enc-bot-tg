90c2f72d1ac42623caa9aea25f3bef4be73beb84
Pavel Tagiltsev <pavel.tagiltsef@gmail.com>
2026-01-22 12:51:14 +0700

    refactor(global): changing folders

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-import CompanyTgBot from './apps/back/CompanyTgBot/CompanyTgBot.js';
+import CompanyTgBot from './server/CompanyTgBot.js';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
